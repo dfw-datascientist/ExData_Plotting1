c90fedf6f4f6add01e92ca81af5a4fcc30663bb3
Jithu Lakshmikanth <jlakshmikanth@jlakshmikanthmbp.averocorp.averoinc.com>
2018-04-09 01:32:41 -0500

    Added comments to distinguish the plots

--- plot4.R
+++ plot4.R
@@ -13,14 +13,24 @@ hpc_set$DateTime <- dmy_hms(hpc_set$DateTime)
 hpc_set$Sub_metering_1 <- as.numeric(hpc_set$Sub_metering_1)
 hpc_set$Sub_metering_2 <- as.numeric(hpc_set$Sub_metering_2)
 hpc_set$Sub_metering_3 <- as.numeric(hpc_set$Sub_metering_3)
+
 png(file = "plot4.png")
 par(mfrow = c(2,2))
+
+# 1st plot
 with(hpc_set, plot(DateTime, Global_active_power, ylab = "Global Active Power", xlab = "", type = "l"))
+
+# 2nd plot
 with(hpc_set, plot(DateTime, Voltage, ylab = "Voltage", xlab = "datetime", type = "l"))
+
+# 3rd plot
 plot(hpc_set$DateTime, hpc_set$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
 points(hpc_set$DateTime, hpc_set$Sub_metering_1, type = "l")
 points(hpc_set$DateTime, hpc_set$Sub_metering_2, type = "l", col = "red")
 points(hpc_set$DateTime, hpc_set$Sub_metering_3, type = "l", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# 4th plot
 with(hpc_set, plot(DateTime, Global_reactive_power, xlab = "datetime", type = "l"))
+
 dev.off()
