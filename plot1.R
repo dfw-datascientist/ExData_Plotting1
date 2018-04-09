055f139ddd3a603dfb20e24dd2022b650ba8f32c
Jithu Lakshmikanth <jlakshmikanth@jlakshmikanthmbp.averocorp.averoinc.com>
2018-04-09 01:17:53 -0500

    Corrected the code to generate hpc dataframe

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 zipfile <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", zipfile)
 unzip(zipfile)
-hpc <- read.table('household_power_consumption.txt', sep = ";", stringsAsFactors = FALSE, header = TRUE, )
+hpc <- read.table(file = 'household_power_consumption.txt', sep = ";", stringsAsFactors = FALSE, header = TRUE)
 #install.packages("lubridate")
 #install.packages("dplyr")
 library(lubridate)
